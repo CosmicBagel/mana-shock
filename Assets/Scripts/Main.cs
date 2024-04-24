044354e0ab7cdc9e154c1b4946e0985959372cd8
CosmicBagel <>
2024-04-23 18:29:19 -0600

    Removed the frame cap, as it caused mouse movement issues in the editor

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,17 +4,6 @@ namespace CosmicBagel
 {
     public class Main : MonoBehaviour
     {
-        // Start is called before the first frame update
-        public void Start()
-        {
-            Debug.Log("hi");
-            Application.targetFrameRate = 60;
-        }
-
-        // Update is called once per frame
-        public void Update()
-        {
-
-        }
+        public void Start() { }
     }
 }
